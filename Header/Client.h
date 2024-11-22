36d3e19f5d049c926f607dd331958020ae11cf92
dilettad <dilettadido@gmail.com>
2024-11-22 17:39:09 +0100

    Casino con make file e giocatore aggiornato?

--- Header/Client.h
+++ Header/Client.h
@@ -1,18 +1,4 @@
 #include "../Header/macro.h"
 #include "../Header/Comunicazione.h"
 #include "../Header/Lista.h"
-
-//Funzione per aggiungere client alla lista
-void add_client(Client** head, int client_fd, char*username);
-
-//Funzione per registrazione del cliente
-void registrazione_client(int client_fd,char* username);
-
-//Funzione per controllare i caratteri dell'username
-int controlla_caratteri(const char* username);
-
-//Funzione per stampare la lista dei clienti
-void stampa_lista_clienti();
-
-// Funzione per liberare la memoria della lista
-void libera_lista_clienti();
+#include "../Header/Giocatore.h"
